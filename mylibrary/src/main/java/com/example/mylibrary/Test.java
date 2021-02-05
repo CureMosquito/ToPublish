54f1e47331ce4b356fffbe2115eff45a87a6e7a0
lids1211 <lids1211@thundersoft.com>
2021-02-05 14:51:22 +0800

    add 2

--- mylibrary/src/main/java/com/example/mylibrary/Test.java
+++ mylibrary/src/main/java/com/example/mylibrary/Test.java
@@ -1,7 +1,24 @@
 package com.example.mylibrary;
 
 public class Test {
+
+    /**
+     * 整数加法.
+     * @param a1 number
+     * @param a2 number
+     * @return number
+     */
     public static int plus(int a1,int a2){
         return a1 + a2;
+    }
+
+    /**
+     * 整数减法.
+     * @param a1 number
+     * @param a2 number
+     * @return number
+     */
+    public static int sub(int a1,int a2){
+        return a1 - a2;
     }
 }
